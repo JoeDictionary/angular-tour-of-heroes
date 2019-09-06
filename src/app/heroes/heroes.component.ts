be4a62339842eb88eb3160f4351cee5ae847dc70
JoeDictionary <filipfit6@googlemail.com>
2019-09-06 23:21:19 +0200

    test commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from "../hero"
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES
+  selectedHero: Hero
+  onSelect(hero:Hero) {
+    this.selectedHero = hero
+  }
+
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
